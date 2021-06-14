87e695daa9a0742d508b4d99f0bcb103e72caa05
wangyang-o <1370228031@qq.com>
2021-06-15 00:10:22 +0800

    轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,9 +5,28 @@
  * @LastEditTime: 2021年06月13日
  */
 Page({
-	data: {},
+	data: {
+		// 轮播图
+		swiperList: [],
+	},
 	//options(Object)
-	onLoad: function (options) {},
+	onLoad: function (options) {
+		let reqTask = wx.request({
+			url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+			data: {},
+			header: { 'content-type': 'application/json' },
+			method: 'GET',
+			dataType: 'json',
+			responseType: 'text',
+			success: (result) => {
+				this.setData({
+					swiperList:result.data.message,
+				});
+			},
+			fail: () => {},
+			complete: () => {},
+		});
+	},
 	onReady: function () {},
 	onShow: function () {},
 	onHide: function () {},
