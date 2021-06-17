d1bb5b4d52b0c2b49f129f1aee60604ca4309b89
wangyang-o <1370228031@qq.com>
2021-06-17 17:48:48 +0800

    工具栏，购物车

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,85 @@
 // pages/cart/index.js
+import { getSetting, chooseAddress, openSetting, showModal, showToast } from "../../utils/asyncWx.js";
 Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		//收货地址
+		address: {},
+		//购物车
+		cart: [],
+		// 全选状态
+		allChecked: true,
+		//总价
+		totalPrice: 0,
+		//总数量
+		totalNum: 0,
+	},
+	//添加收货地址事件
+	async addressChoose() {
+		try {
+			const scope = await getSetting();
+			if (scope === false) {
+				await openSetting();
+			}
+			let address = await chooseAddress();
+			address.all =
+				address.provinceName +
+				address.cityName +
+				address.countyName +
+				address.detailInfo;
+			wx.setStorageSync('address', address);
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {},
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {},
 
-  },
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {
+		//收货地址接收
+		const address = wx.getStorageSync('address');
+		this.setData({
+			address,
+		});
+		//购物车接收
+		const cart = wx.getStorageSync('cart') || [];
+		// this.setCart(cart);
+	},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {},
 
-  },
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {},
 
-  },
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {},
+});
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -42,6 +42,32 @@ Page({
 			urls, // 需要预览的图片http链接列表
 		});
 	},
+	// 点击加入购物车
+	handleCartAdd() {
+		let cart = wx.getStorageSync('cart') || [];
+		let index = cart.findIndex((item) => {
+			return item.goods_id === this.goodsInfo.goods_id;
+		});
+		if (index === -1) {
+			// 不存在
+			this.goodsInfo.num = 1;
+			cart.push(this.goodsInfo);
+			wx.showToast({
+				title: '添加购物车成功',
+				icon: 'success',
+				mask: true,
+			});
+		} else {
+			// 存在
+			cart[index].num++;
+			wx.showToast({
+				title: '商品数量+1',
+				icon: 'success',
+				mask: true,
+			});
+		}
+		wx.setStorageSync('cart', cart);
+	},
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
